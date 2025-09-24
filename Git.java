fc8f93d5810aee7afc0ab5cd169d78dd2b3d4066
Hunter Madden <hmadden1@hwemail.com>
2025-09-24 12:59:00 -0700

    (GP-2.2): Created a Hash Function
    Uses MessageDigest to create a SHA-1 Hash for a file, given a Path. See README for more details.

--- Git.java
+++ Git.java
@@ -1,11 +1,19 @@
 import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.math.BigInteger;
 public class Git{
     public void InitRepo(){
+        //basic Git files required for initialization
         File git = new File("git");
         File objects = new File("git/objects");
         File index = new File("git/index");
         File head = new File("git/HEAD");
+        //If there is going to be an issue, its going to be here
         if (!git.exists() && !objects.exists() && !index.exists() && !head.exists()){
+           //basic-ass file creation
             git.mkdir();
             objects.mkdir();
             try{
@@ -14,10 +22,33 @@ public class Git{
             }catch(IOException e){
                 e.printStackTrace();
             }
+            //output message
             System.out.println("Git Repository Created");
         }else{
             System.out.println("Git Repository Already Exists");
         }
 
+    }
+    public String HashFile(Path filePath){
+        try{
+        //SHA-1 initialization
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        //byte array created here
+        byte[] intermediaryArray = md.digest(Files.readString(filePath, StandardCharsets.UTF_8).getBytes(StandardCharsets.UTF_8));
+        BigInteger hashResult = new BigInteger(1, intermediaryArray);
+        StringBuilder output = new StringBuilder(hashResult.toString(16));
+        //fills out trailing zeros on the output string
+        while (output.length() < 40)
+        {
+            output.insert(0, '0');
+        }
+        return output.toString();
+        //exception handeling
+    } catch(NoSuchAlgorithmException e){
+            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return "";
     }
 }
