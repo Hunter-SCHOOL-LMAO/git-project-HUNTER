2fa6ef17b37e4fa05486271283517454901d732f
Hunter Madden <hmadden1@hwemail.com>
2025-09-24 12:45:14 -0700

    (GP-2.1): Program Can Initialize Repository
    see README file for new method description

--- Git.java
+++ Git.java
@@ -1,3 +1,23 @@
+import java.io.*;
 public class Git{
+    public void InitRepo(){
+        File git = new File("git");
+        File objects = new File("git/objects");
+        File index = new File("git/index");
+        File head = new File("git/HEAD");
+        if (!git.exists() && !objects.exists() && !index.exists() && !head.exists()){
+            git.mkdir();
+            objects.mkdir();
+            try{
+                 index.createNewFile();
+                head.createNewFile();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            System.out.println("Git Repository Created");
+        }else{
+            System.out.println("Git Repository Already Exists");
+        }
 
+    }
 }
