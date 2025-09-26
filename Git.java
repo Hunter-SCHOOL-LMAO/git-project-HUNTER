a0fd60f648884670ae134e855c2f4466284f0e2b
Hunter Madden <hmadden1@hwemail.com>
2025-09-26 09:52:03 -0700

    resolving merge conflicts
    git is weird

--- Git.java
+++ Git.java
@@ -5,11 +5,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.math.BigInteger;
 public class Git{
-<<<<<<< HEAD
     public void initRepo(){
-=======
-    public void InitRepo(){
->>>>>>> origin/HashBrowns
         //basic Git files required for initialization
         File git = new File("git");
         File objects = new File("git/objects");
@@ -33,20 +29,12 @@ public class Git{
         }
 
     }
-<<<<<<< HEAD
     public String hashFile(byte[] contents){
-=======
-    public String HashFile(Path filePath){
->>>>>>> origin/HashBrowns
         try{
         //SHA-1 initialization
         MessageDigest md = MessageDigest.getInstance("SHA-1");
         //byte array created here
-<<<<<<< HEAD
         byte[] intermediaryArray = md.digest(contents);
-=======
-        byte[] intermediaryArray = md.digest(Files.readString(filePath, StandardCharsets.UTF_8).getBytes(StandardCharsets.UTF_8));
->>>>>>> origin/HashBrowns
         BigInteger hashResult = new BigInteger(1, intermediaryArray);
         StringBuilder output = new StringBuilder(hashResult.toString(16));
         //fills out trailing zeros on the output string
@@ -63,7 +51,6 @@ public class Git{
         }
         return "";
     }
-<<<<<<< HEAD
     public void makeBlob(byte[] contents){
         String fileName = hashFile(contents);
         File output = new File("/git/objects/"+fileName);
@@ -71,6 +58,4 @@ public class Git{
         Files.write(Paths.get("/git/objects/"+fileName), contents);
         
     }
-=======
->>>>>>> origin/HashBrowns
 }
