622b65285b2c31fb9c3c037d64903ea95a1824b5
Hunter Madden <hmadden1@hwemail.com>
2025-10-06 23:26:24 -0700

    (GP-3.3): Creating a Tree from the Index (#6)
    * (GP-3.2): Creating a Basic Tree
    
    created methods fileTree and fileTreeRecursive. See README for more details.
    
    * (GP-3.3): Creating a Tree from the Index
    
    New method to generate a tree from just the index file. probably has some issues, but im too tired to fix them. See README for more details.

--- Git.java
+++ Git.java
@@ -3,12 +3,14 @@ import java.nio.file.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.math.BigInteger;
+import java.util.*;
 public class Git{
     //instance variables
     private File git = new File("git");
     private File objects = new File("git/objects");
     private File index = new File("git/index");
     private File head = new File("git/HEAD");
+    private LinkedList<WorkingListObject> workingList;
     //getters
     public File getGit(){
         return git;
@@ -22,6 +24,7 @@ public class Git{
     public File getHead(){
         return head;
     }
+    //lmao no setters
     public void initRepo(){
         //basic Git files required for initialization
         //If there is going to be an issue, its going to be here
@@ -80,16 +83,17 @@ public class Git{
         String line = "";
         String newHash = hashFile(Files.readAllBytes(filePath));
         int lineOffset = 0;
+        //big check to see if the filePath already exists in the index
         while(line != null){
             line = lineReader.readLine();
             //isolates the filePath and blob name from the read line
-            String indexFilePath = line.substring(46);
-            String indexBlobName = line.substring(6, 46);
-            //checks and appends the blob name if the file path is different
+            String indexFilePath = line.substring(newHash.length());
+            String indexBlobName = line.substring(0, 41);
+            //checks and appends the blob name if the file name is the same, but the hash is different
             if(indexFilePath.equals(filePath.toString())){
                 if(!hashFile(Files.readAllBytes(filePath)).equals(indexBlobName)){
                     //lineOffset in necessary, or else it will just contine editing the first line
-                    appender.append(newHash, 6+lineOffset, 46+lineOffset);
+                    appender.append(newHash, lineOffset, lineOffset+newHash.length() + 1);
                     lineReader.close();
                     appender.close();
                     return true;
@@ -97,17 +101,18 @@ public class Git{
             }
             lineOffset += line.length();
         }
-        //combines two methods into one, may delete later
+        //For blob-making conveinence, may delete later
         if(createBlob){
             byte[] contents = Files.readAllBytes(filePath);
             makeBlob(contents, newHash);
         }
+        //gets rid of the newline at the beginning of the file
         if(index.length() == 0){
-            String output = "blob "+newHash + " " + filePath.toString();
+            String output = newHash + " " + filePath.toString();
             Files.write(index.toPath(), output.getBytes(), StandardOpenOption.APPEND);
         }
         else{
-             String output = "\nblob "+ newHash + " " + filePath.toString();
+             String output = "\n"+newHash + " " + filePath.toString();
              Files.write(index.toPath(), output.getBytes(), StandardOpenOption.APPEND);
         }
         lineReader.close();
@@ -116,5 +121,109 @@ public class Git{
             e.printStackTrace();
         }
         return true;
+    }
+    public String fileTree(String startPath){
+        return Paths.get(fileTreeRecursive(startPath)).getFileName().toString();
+    }
+    //recursive function for fileTree
+    public String fileTreeRecursive(String startPath){
+        File startingFolder = new File(startPath);
+        File[] fileList = startingFolder.listFiles();
+        StringBuilder output = new StringBuilder();
+        try{
+            for(File e : fileList){
+                Path ePath = e.toPath();
+                //base case
+                if(!e.isDirectory()){
+                    //gets rid of starting newline
+                    if(output.length() != 0){
+                        output.append("blob "+ hashFile(Files.readAllBytes(ePath)) + " " + e.getName());
+                    }else{
+                        output.append("\nblob "+ hashFile(Files.readAllBytes(ePath)) + " " + e.getName());
+                    }
+                }
+                //recursive case
+                else{
+                    //recursive call
+                    File recursedTree = new File(objects + Paths.get(fileTreeRecursive(startPath)).getFileName().toString());
+                    //gets rid of starting newline
+                    if(output.length() != 0){
+                        output.append("tree" + recursedTree.getName() + e.getName());
+                    }
+                    else{
+                    output.append("\ntree" + recursedTree.getName() + e.getName());
+                    }
+                }
+            }
+            //turns the StringBuilder into an actual File
+            File outputTree = new File(objects + hashFile(output.toString().getBytes()));
+            outputTree.createNewFile();
+            Files.write(outputTree.toPath(), output.toString().getBytes());
+            return outputTree.getName();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public void initWorkingList(){
+        ArrayList<WorkingListObject> workingArray = new ArrayList<WorkingListObject>();
+        try{
+            BufferedReader lineReader = Files.newBufferedReader(index.toPath());
+            while(lineReader.ready()){
+                //adds WorkingListObjects (an empty containter class) to the list from the index file
+                String line = lineReader.readLine();
+                workingArray.add(new WorkingListObject("blob", line.substring(0, 41), Paths.get(line.substring(41))));
+            }
+            lineReader.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        //god, I hope this works 
+        Collections.sort(workingArray);
+        workingList = new LinkedList<>(workingArray);
+        recursiveColapse(workingList.getFirst());
+    }
+    ///GRAAAH I HATE IT I HATE IT I HATE IT
+    public void recursiveColapse(WorkingListObject previous){
+        //breakaway boolean
+        boolean stillEqual = true;
+        workingList.removeFirst();
+        //Stringbuilder init
+        StringBuilder outputTree = new StringBuilder(previous.getType() + " " + previous.getHash() + " " + previous.getPath().getFileName());
+        try{
+            //for every object left in the array (if there are none, proceed to the base case)
+            for(int i = 0; i < workingList.size() && stillEqual; i ++){
+                //grab the next item
+                WorkingListObject next = workingList.getFirst();
+                //if the parent is equal to the start of the chain
+                if(next.getPath().getParent().equals(previous.getPath().getParent())){
+                    //add it to the next line on the sub-tree and remove it
+                    outputTree.append("\n" + next.getType() + " " + next.getHash() + " " + next.getPath().getFileName());
+                    workingList.removeFirst();
+                }
+                /*ELSE EXPLANATION
+                 * IF THE NEXT FILE DOES NOT SHARE THE SAME PARENT DIRECTORY
+                 * DO NOT TRIGGER BASE CASE
+                 * SAVE THE CURRENT TREE
+                 * START THE PROCESS OVER AGAIN WITH THE CURRENT TREE AS THE START OF NEXT RECURSE
+                 * IF THE SORTING ACTUALLY *WORKS*, THE NEXT RECURSION SHOULD BE THE STUFF IMMEDIATELY OUTSIDE THE CURRENT DIRECTORY
+                 */
+                else{
+                    stillEqual = false;
+                    File leafTree = new File(objects + hashFile(outputTree.toString().getBytes()));
+                    Files.write(leafTree.toPath(), outputTree.toString().getBytes());
+                    workingList.addFirst(new WorkingListObject("tree", hashFile(outputTree.toString().getBytes()), previous.getPath().getParent()));
+                    recursiveColapse(next);
+                }
+            }//base case
+            if(stillEqual = true){
+                //save the file (and update the directory i guess)
+                File rootTree = new File(objects + hashFile(outputTree.toString().getBytes()));
+                Files.write(rootTree.toPath(), outputTree.toString().getBytes());
+                workingList.addFirst(new WorkingListObject("tree", hashFile(outputTree.toString().getBytes()), previous.getPath().getParent()));
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ WorkingListObject.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.nio.file.*;
+public class WorkingListObject implements Comparable<WorkingListObject>{
+    private String type;
+    private String hash;
+    private Path path;
+    public WorkingListObject(String type, String hash, Path path){
+        this.type = type;
+        this.hash = hash;
+        this.path = path;
+    }
+    //getters and setters
+    public String getType(){
+        return type;
+    }
+    public String getHash(){
+        return hash;
+    }
+    public Path getPath(){
+        return path;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setHash(String hash){
+        this.hash = hash;
+    }
+    public void setPath(Path path){
+        this.path = path;
+    }
+    public int compareTo(WorkingListObject other){
+        return path.compareTo(other.getPath());
+    }
+}
